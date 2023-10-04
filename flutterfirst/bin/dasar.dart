b3cd99158d8666020bfaec590c494a3922c3a950
Zeyynal <arifinzain291@gmail.com>
2023-10-04 13:41:24 +0800

    first commit

---
+++ flutterfirst/bin/dasar.dart
@@ -0,0 +1,11 @@
+void main () {
+  String nama = "zain";
+  nama += "mcom";
+
+  num umur = 22;
+  bool ismenikah = true;
+
+  print("nama saya $nama");
+  print("umur saya $umur");
+  print("saya sudah menikah = $ismenikah");
+}
