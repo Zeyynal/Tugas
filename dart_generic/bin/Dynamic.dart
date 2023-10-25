bf87fae6b426c141e59a07989480aef8ff2166f3
Zeyynal <arifinzain291@gmail.com>
2023-10-25 14:59:45 +0800

    Quiz

---
+++ dart_generic/bin/Dynamic.dart
@@ -0,0 +1,11 @@
+import 'MyData.dart';
+
+void printData(MyData data) {
+  print(data.data);
+}
+
+void main(){
+  printData(MyData("Zainal"));
+  printData(MyData(100));
+  printData(MyData(true));
+}
